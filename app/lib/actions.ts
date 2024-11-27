95639bc65faacc385c735b51aa0d4d4c42b7d875
Ruslan <lisson87@mail.ru>
2024-11-27 10:12:17 +0300

    chapter 12 after update invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,10 +24,7 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-    // Test it out:
-    // console.log(rawFormData);
-    // console.log(typeof rawFormData.amount);
-
+    
     // вставка значений в БД
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
@@ -35,6 +32,27 @@ export async function createInvoice(formData: FormData) {
   `;
 
   revalidatePath('/dashboard/invoices');
-
   redirect('/dashboard/invoices');
+  }
+
+  // Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+  export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
   }
